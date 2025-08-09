a55e77ccc59cca147e8c7c589f01446b2dc2ff6e
Manoj Kharkar <manojkharkar909@gmail.com>
2025-08-09 19:51:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all JS code as never version
+
+// alert("hello") we are using node.js not browser
+
+let name = "manoj"
+let age = 20
+let isSmart = true
+
+
+//number 
+//bigint
+//string
+// boolean
+//null => standalone value
+// undefined
+//symbol => determine unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);  //indefined
