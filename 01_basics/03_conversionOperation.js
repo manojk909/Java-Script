0187d774bdaa123547e8a6684609b9e21bd1d085
Manoj Kharkar <manojkharkar909@gmail.com>
2025-08-22 01:26:24 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,3 +8,18 @@ console.log(typeof valInNumber);
 // "33" => 33
 // "33abc" => NaN
 // true => 1
+//null => 0; 
+//undefined => NaN
+//"manoj" => NaN
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+
+let num = 99
+let strn = String(num)
+console.log(strn)
+console.log(typeof strn)
