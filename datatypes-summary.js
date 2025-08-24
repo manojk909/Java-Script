dece2b2a6cea7dfb8ec38385e08c65c5d36b4345
Manoj Kharkar <manojkharkar909@gmail.com>
2025-08-24 10:07:48 +0000

    datatype

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+ // Primitive
+
+ // 7 types : String, Numbers, Boolean, null, Undefined, Symbol, BigInt
+
+const sc= 100
+const isLog = false
+let userEmail
+
+const id = Symbol("123")
+const amID = Symbol("123")
+console.log(id === amID);
+
+const bigNumber = 123687412685n
+
+
+
+ //# Reference (NON Premitive)
+ //Arrays, Objects, functions
+
+ const heros = ["shaktiman", "naagraj", "doga"]
+
+let myobj =  {
+    name: "mk",
+     age: 21,
+ }
+
+ const myfunction = function(){
+    console.log("Hello world");
+    
+ }
+
+ //DATA TYPE OF ARR, FUNCT------> function
