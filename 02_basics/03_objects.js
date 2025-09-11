ac8124f381bd2c45523e6bf25dd7f1aa4c111732
Manoj Kharkar <manojkharkar909@gmail.com>
2025-09-11 10:25:01 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,12 +1,38 @@
-//singolton
+// singleton
+// Object.create
 
-//object literals
+// object literals
+
+const mySym = Symbol("key1")
+
 
 const JsUser = {
-    name: "Manoj",
-    age: 21,
-    location: "Lucknow",
-    email: "manojk123@gmail.com",
-    isLogged: false,
-    
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
