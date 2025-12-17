5ef1b76f785ce585ea5d5757e12dff7ffc1733c0
Manoj Kharkar <manojkharkar909@gmail.com>
2025-12-17 09:48:06 +0000

    if else

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -11,16 +11,19 @@ const month = "march"
 
 switch (month) {
     case "jan":
-        console.log(1);
+        console.log("January");
         break;
     case "feb":
-        console.log(2);
+        console.log("feb");
         break;
     case "march":
-        console.log(3);
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
         break;
 
     default:
-        console.log("default msg");
+        console.log("default case match");
         break;
 }
