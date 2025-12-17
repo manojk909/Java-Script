c14e53e6172211eece4e2c57d3eb122de74a34c5
Manoj Kharkar <manojkharkar909@gmail.com>
2025-12-17 09:46:17 +0000

    if else, nullish and turnery perator

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+//if 
+
+const isUserlogIn = true
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+if (2=="2"){
+    console.log("Executed");
+}
+if (2!=="2"){
+    console.log("Executed");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log(1);
+        break;
+    case "feb":
+        console.log(2);
+        break;
+    case "march":
+        console.log(3);
+        break;
+
+    default:
+        console.log("default msg");
+        break;
+}
