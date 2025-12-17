510ffeb2e760cf45c8ec9e48cee2df6552ac7325
Manoj Kharkar <manojkharkar909@gmail.com>
2025-12-17 09:09:35 +0000

    iife explaine

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,11 +1,12 @@
 
 // Immediately Invoked Function Expressions (IIFE)
-
+// func which execute immediately
+// global scopeke polution ko hatane ke liye 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();   //yha func end karo (;) 
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
